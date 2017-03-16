cd1ac94cec6b38dd21bfff05ddefa304a299fcce
Antoine Chauvin <blackrushx@gmail.com>
2017-03-16 17:21:14 +0100

    Evaluate variable expression

--- seance 03/eval.ml
+++ seance 03/eval.ml
@@ -1,8 +1,24 @@
 open Types ;;
+open Dict ;;
+open Maybe ;;
 
 exception Eval_exn of string ;;
 
-let evaldbg x =
-  print_string ((print_expression x) ^ "\n");
-  []
+let isval expr =
+  match expr with
+  | Function (_, _, _) -> true
+  | _ -> false
 ;;
+
+let eval expr ctx =
+  match expr with
+
+  | Variable varname ->
+    let result = maybe_get (dict_get varname ctx) (Eval_exn ("unknown variable " ^ varname)) in
+    print_string ((print_expression result) ^ "\n");
+    ctx
+
+  | _ ->
+    raise (Eval_exn "invalid expression")
+
+  ;;
--- seance 03/main.ml
+++ seance 03/main.ml
@@ -2,14 +2,18 @@ open Types ;;
 open Dict ;;
 open Eval ;;
 
+let debug_expression x =
+  print_string ((print_expression x) ^ "\n")
+;;
+
 let rec loop ctx =
   try
     print_string ">>> ";
 
     let expr = Parser.line Lexer.lexer ((Lexing.from_string (read_line () ^"\n"))) in
 
-    (* let new_ctx = eval expr ctx in *)
-    let new_ctx = evaldbg expr in
+    debug_expression expr;
+    let new_ctx = eval expr ctx in
 
     print_string ((dict_str (dict_map_values print_expression new_ctx)) ^ "\n");
 
