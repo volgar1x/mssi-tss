92f2b8a9310fa68a9915dde9d6c6dd5816d8b8ad
Antoine Chauvin <blackrushx@gmail.com>
2017-03-16 17:52:29 +0100

    Evaluate function application

--- seance 03/eval.ml
+++ seance 03/eval.ml
@@ -10,14 +10,24 @@ let isval expr =
   | _ -> false
 ;;
 
-let eval_expr expr ctx =
+let rec eval_expr expr ctx =
   match expr with
 
   | Variable varname ->
     maybe_get (dict_get varname ctx) (Eval_exn ("unknown variable " ^ varname))
 
-  | Function (_, _, _) ->
-    expr
+  | Function (paramname, body, inner_ctx) ->
+    let new_ctx = dict_merge ctx inner_ctx in
+    Function (paramname, body, new_ctx)
+
+  | Application (fnexpr, paramexpr) ->
+    let paramresult = eval_expr paramexpr ctx in
+
+    match eval_expr fnexpr ctx with
+    | Function (paramname, body, inner_ctx) ->
+      let eval_ctx = dict_put paramname paramresult inner_ctx in
+      eval_expr body eval_ctx
+    | _ -> raise (Eval_exn "")
 
   | _ ->
     raise (Eval_exn "TODO")
@@ -27,16 +37,13 @@ let eval_expr expr ctx =
 let eval expr ctx =
   match expr with
 
-  | Variable _ ->
-    let result = eval_expr expr ctx in
-    print_string ((print_expression result) ^ "\n");
-    ctx
-
   | Assignation (Variable varname, varexpr) ->
     let varresult = eval_expr varexpr ctx in
     dict_put varname varresult ctx
 
   | _ ->
-    raise (Eval_exn "TODO")
+    let result = eval_expr expr ctx in
+    print_string ((print_expression result) ^ "\n");
+    ctx
 
   ;;
